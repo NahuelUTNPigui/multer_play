9eded80cc3e35f977388f2c837a7b4b5de8975fc
nahuel facu <nahuel.utn.frc.piguillem@gmail.com>
2023-10-07 15:53:09 -0300

    test

--- main.js
+++ main.js
@@ -4,17 +4,43 @@ const app = express()
 var path = require('path');
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
+        console.log("DEST")
         cb(null,path.join(__dirname,'public'))
     },
     filename:function(req,file,cb){
+        console.log("name")
         cb(null,file.fieldname+"-"+Date.now()+".png")
     }
 })
 const upload = multer({ storage:storage ,limits: { fileSize: 5 * 1024 * 1024 },})
+const fupfoto= multer({ storage:storage ,limits: { fileSize: 5 * 1024 * 1024 },}).single('foto')
+let fup=function(req,res){
+    fupfoto(req,res,function(err){
+        if (err){
+            console.log("erro")
+            console.log(err)
+        }
+    })
+    next()
+}
 app.use('/public', express.static('public'));
 app.post('/foto', upload.single('foto'), function (req, res, next) {
     console.log(req.body)
     console.log(req.file)
+    res.json("todo bien")
+})
+app.post('/foto2',function(req,res){
+    fupfoto(req,res,function(err){
+        if (err instanceof multer.MulterError) {
+            console.log("mult error")
+            console.log(err)
+            // A Multer error occurred when uploading.
+          } else if (err) {
+            console.log("err")
+            console.log(err)
+            // An unknown error occurred when uploading.
+          }
+    })
     res.json("todo bien")
 })
 app.listen(4000,()=>{
